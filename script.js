16639f856d910660d3989c5e3b94440a1fbc7407
victorfuhr-beep <victor.fuhr@escola.pr.gov.br>
2026-06-12 10:23:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// script.js
 
+const botao = document.getElementById("btnMensagem");
+
+botao.addEventListener("click", () => {
+  alert(
+    "O agro sustentável protege o meio ambiente e garante alimento para o futuro! 🌱"
+  );
+});
